6b13808c2de6025d11095896744a2908c0479afa
jihoon <blue_jihoon@naver.com>
2020-03-23 21:23:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Game of Life - Streami
+
+사용언어: Python 3.7.2
+
---
+++ game_of_life/game_of_life.py
@@ -0,0 +1,43 @@
+"""
+python을 이용한 game of life
+
+주요 프로세스
+- 셋팅
+    - 파일을 읽어오거나 입력값을 처리하는 부분
+    - 표 생성(최소 width 80, height 40)
+
+- 세포의 세대별 변화
+    - 세대에 따라 세포의 변화
+    - 네 개의 조건을 바탕으로 알고리즘 구현
+
+- 화면 출력
+    - 어떻게 화면에 출력할 것인가?
+    - 모든 세대가 완료된 후 txt파일로 세포의 변화 저장
+"""
+
+
+# 파일과 세대는 주어질 수 있다.
+def initialize(file=None, gen=None):
+    # 파일 불러오기
+    # 표 생성
+    pass
+
+
+# 초기 셀 -> change_generations(초기 셀) -> 
+#
+def change_generations(prev_cell):
+    # 세대에 따라 셀의 변화
+    # 네 개의 조건을 바탕으로 알고리즘 구현
+    next_cell = []
+    return next_cell
+
+
+def visualize():
+    # 화면에 출력
+    pass
+
+
+def main():
+    # 게임 실행
+    # 게임 완료 후 txt파일로 저장
+    pass
