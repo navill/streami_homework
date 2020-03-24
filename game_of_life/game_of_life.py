ff3fb75b94f4f33e86dfffbca44656df616ad84a
jihoon <blue_jihoon@naver.com>
2020-03-24 23:38:40 +0900

    세대별 세포 변화 출력 기능 구현

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,14 +1,21 @@
+from copy import deepcopy
+
 ##################
+# MAIN_GRID: 화면에 뿌려질때 사용될 grid 변수
 MAIN_GRID = []
+# CONTAINER_GRID: 이전 세대 grid를 잠시 담고 있을 변수. 조건 연산에 사용됨
+CONTAINER_GRID = []
 # default
 SIZE = [80, 40]
 # 중앙 셀을 기준으로 이웃을 검사하기 위해 사용
 ##################
-
-
 # size에 맞는 빈('chr(9633 -> □)') 2차 배열 구현
 for _ in range(SIZE[1]):
     MAIN_GRID.append([''.join(chr(9633))] * SIZE[0])
+
+
+for _ in range(SIZE[1]):
+    CONTAINER_GRID.append([''.join(chr(9633))] * SIZE[0])
 
 
 def initialize(filename=None, gen=None):
@@ -61,13 +68,17 @@ def create_grid(cell_coord_list):
     """
     :param <list> cell_coord_list: 세포들의('■') 좌표가 들어있는 리스트
     """
-    for row, col in cell_coord_list:
-        MAIN_GRID[row][col] = chr(9632)
+
+    for x, y in cell_coord_list:
+        MAIN_GRID[x][y] = chr(9632)
+        CONTAINER_GRID[x][y] = chr(9632)
+        print('create_grid:', x, y)
 
     return cell_coord_list
 
 
 def check_live_neighbor(cell_row, cell_col):
+    # print('checklive:', cell_row, cell_col)
     """
     살아있는 세포의 주변 세포가 살아있는지 측정(조건 1,2,3)
     :param <int> cell_row: 체크하고자 하는 셀의 행
@@ -75,17 +86,25 @@ def check_live_neighbor(cell_row, cell_col):
     """
     live_cell_num = 0
     # 이웃 셀 좌표 생성
-
-    for y in [cell_col - 1, cell_col, cell_col + 1]:
-        for x in [cell_row - 1, cell_row, cell_row + 1]:
-            if x == cell_col and y == cell_row:
+    for row in [cell_row - 1, cell_row, cell_row + 1]:
+        # print('y:', row)
+        for col in [cell_col - 1, cell_col, cell_col + 1]:
+            # print('x:', col)
+            # 자신을 제외하고 순회
+            # print('5,4:', test_grid[cell_row][cell_col], col, row)
+            if col == cell_col and row == cell_row:
                 continue
+                # size를 벗어나는 index는 무시
             try:
                 # 주변에 살아있는 세포가 있을 경우
-                if MAIN_GRID[x][y] == '■':
+                if CONTAINER_GRID[row][col] == '■':
                     live_cell_num += 1
+                    # print(cell_row, cell_col)
+                    # print(col, row)
+                    # print('------------')
             except:
                 pass
+
     return live_cell_num
 
 
@@ -101,24 +120,39 @@ def check_live_neighbor(cell_row, cell_col):
 """
 
 
-def change_generation(cell_list):
+def change_generation():
+    global CONTAINER_GRID
+    old_grid = CONTAINER_GRID
     """
     :param <list> 다음 세대로 변하기 전 살아 있는 세포들의 좌표 리스트
     """
+    # 이전 세대 세포가 그려진 표
+
+    # new = old.copy() -> change_generation에서 new를 사용-> old = new
+
     # col부터 진행 -> cache hit 유도
-    for col in range(len(MAIN_GRID)):
-        for row, cell in enumerate(MAIN_GRID[col]):
+    for row in range(len(old_grid)):
+        for col, cell in enumerate(old_grid[row]):
             live_neighbor_cell_num = check_live_neighbor(row, col)
+            if live_neighbor_cell_num != 0:
+                print(live_neighbor_cell_num, row, col)
             if '■' == cell:
                 if live_neighbor_cell_num == 1 or live_neighbor_cell_num == 0:  # 조건1
                     MAIN_GRID[row][col] = '□'  # die
+                    print('condition1-die:', row, col)
                 elif live_neighbor_cell_num > 3:  # 조건2
                     MAIN_GRID[row][col] = '□'  # die
+                    print('condition2-die:', row, col)
+
                 elif live_neighbor_cell_num == 2 or live_neighbor_cell_num == 3:  # 조건3
+                    print('keep:', row, col)
                     continue  # 살아있는 상태 유지
             else:
                 if live_neighbor_cell_num == 3:
-                    MAIN_GRID[col][row] = '■'  # resurrection
+                    print('resurrection:', row, col)
+                    MAIN_GRID[row][col] = '■'  # resurrection
+
+    CONTAINER_GRID = deepcopy(MAIN_GRID)
 
 
 def visualize():
@@ -134,20 +168,20 @@ def main():
 
 if __name__ == '__main__':
     # 초기화
-    # initialize()  # 아무 값을 입력하지 않을 경우 -> random state
-    # initialize('plus.txt')  # 파일 이름과 세대를 입력할 경우 -> 파일에 정의된 state
-
-    # 초기화
     s, g, i = initialize('plus.txt')  # 사용자가 값을 입력하지 않을 경우
+
     # 그리드 생성
     result = create_grid(i)  # 배열을 이용해 그리드 생성
+    first_grid = []
+    for i in MAIN_GRID:
+        first_grid.append(''.join(i))
+    print(first_grid)
 
-    test_result1 = []
-    for i in MAIN_GRID:
-        test_result1.append(''.join(i))
-    print(test_result1)
-    change_generation(i)
-    test_result = []
-    for i in MAIN_GRID:
-        test_result.append(''.join(i))
-    print(test_result)
+    # 세포의 세대별 변화 출력
+    for _ in range(7):
+        change_generation()
+        test_result = []
+        for i in MAIN_GRID:
+            test_result.append(''.join(i))
+        print(test_result)
+
