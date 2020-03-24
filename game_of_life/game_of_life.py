59d8eb0093bb1cc9cba7f58d65623349bb73541d
jihoon <blue_jihoon@naver.com>
2020-03-24 19:44:26 +0900

    grid 생성 및 주변 이웃이 살아있는지 체크하는 함수 생성(세대 변경 함수 작성중)

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,35 +1,24 @@
-"""
-python을 이용한 game of life
-
-주요 프로세스 - 초안
-- 셋팅(initialize)
-    - 파일을 읽어오거나 입력값을 처리하는 부분
-
-- 표 생성(create_grid)
-    - 최소 width 80, height 40
-
-- 세포의 세대별 변화
-    - 세대에 따라 세포의 변화
-    - 네 개의 조건을 바탕으로 알고리즘 구현
-
-- 화면 출력
-    - 어떻게 화면에 출력할 것인가?
-    - 모든 세대가 완료된 후 txt파일로 세포의 변화 저장
-
-main -> initialize -> create_grid -> change_generations -> visualize(Tkinter)
-"""
+##################
+MAIN_GRID = []
+# default
+SIZE = [80, 40]
+# 중앙 셀을 기준으로 이웃을 검사하기 위해 사용
+##################
 
 
-# 파일과 세대는 주어질 수 있다.
-# Test => initialize('plus.txt')
+# size에 맞는 빈('chr(9633 -> □)') 2차 배열 구현
+for _ in range(SIZE[1]):
+    MAIN_GRID.append([''.join(chr(9633))] * SIZE[0])
+
+
 def initialize(filename=None, gen=None):
     """
-    :param filename: 사용될 파일 이름(plus.txt)
-    :param gen: 사용자가 입력할 세대의 수
+        파일과 세대는 주어질 수 있다.
+        :param <str> filename: 사용될 파일 이름(plus.txt)
+        :param <int> gen: 사용자가 입력할 세대의 수
     """
-
     # default size
-    height, width = 80, 40
+    height, width = SIZE
     init_cell = []
     # 파일이 있을 경우 -> 파일을 기반으로 초기화
     if filename:
@@ -59,7 +48,7 @@ def initialize(filename=None, gen=None):
         # 랜덤으로 생성할 좌표의 수
         rand_cell = random.randint(1, 9)
         gen = random.randint(1, 9)
-        # 생성 가능한 좌표의 리스트 - [19,19]~ [21,21]
+        # 생성 가능한 좌표의 리스트 - [19,19]~ [21,21]  // 3x3=9칸에 임의로 세포 생성
         init_cell = list(itertools.product(range(19, 22), repeat=2))
         random.shuffle(init_cell)
 
@@ -68,56 +57,68 @@ def initialize(filename=None, gen=None):
     return [int(width), int(height)], gen, init_cell
 
 
-# Test => size, gen, init_cell = initialize('plus.txt')
-# create_grid(size, gen, init_cell)
-def create_grid(size, init_cell):
+def create_grid(cell_coord_list):
     """
-    :param size: 초기화된 grid의 크기
-    :param init_cell: 초기화된 cell
+    :param <list> cell_coord_list: 세포들의('■') 좌표가 들어있는 리스트
     """
-    # size에 맞는 빈('chr(9633 -> □)') 2차 배열 구현
-    grid_list = []
-    for _ in range(size[1]):
-        grid_list.append([''.join(chr(9633))] * size[0])
+    for row, col in cell_coord_list:
+        MAIN_GRID[row][col] = chr(9632)
 
-    for row, col in init_cell:
-        grid_list[row][col] = chr(9632)
-    # default(80x40) 사이즈의 리스트 반환
-    return grid_list
+    return cell_coord_list
 
 
-# 초기 세포(init_cell)를 initialize()에서 받아옴 -> 세포 변환을 change_generations에서 진행하고 결과 전달
-# -> change_grid()함수를 새로 생성해서 새로운 그리드 생성 -> 세포 변환 -> 그리드 생성 -> 반복
-def change_grid(next_cell):
-    return 0
+def check_live_neighbor(cell_row, cell_col):
+    """
+    살아있는 세포의 주변 세포가 살아있는지 측정(조건 1,2,3)
+    :param <int> cell_row: 체크하고자 하는 셀의 행
+    :param <int> cell_col: 체크하고자 하는 셀의 열
+    """
+    live_cell_num = 0
+    # 이웃 셀 좌표 생성
+
+    for y in [cell_col - 1, cell_col, cell_col + 1]:
+        for x in [cell_row - 1, cell_row, cell_row + 1]:
+            if x == cell_col and y == cell_row:
+                continue
+            try:
+                # 주변에 살아있는 세포가 있을 경우
+                if MAIN_GRID[x][y] == '■':
+                    live_cell_num += 1
+            except:
+                pass
+    return live_cell_num
 
 
 """
-def to_do():
-    sizw, gen, init_cell = initialize()  # initalize에서 init_cell 반환
-    next_grid = create_grid(size, init_cell)  # 초기 grid 생성
-    prev_grid = init_cell
-    
-    while g:  # generation 횟수가 남아있을 때 까지 반복
-        next_generation = change_generation(next_cell)  # 1.처음에 초기값 init_cell을 받아오고 다음 세대 생성, 4. 다음 세대 생성
-        next_grid = change_grid(prev_grid, next_generation)  # 2. 다음 세대를 이용해 그리드 변화
-        
-        next_cell = next_generation  # 3. next_cell에 next_generation을 그대로 반환
-        prev_grid = next_grid
-        g -= 1
-        if g == 0:
-            break
-            
-# change_grid 함수 생성
-# change_generation 함수 생성
+네 개의 조건을 바탕으로 알고리즘 구현
+공간이 채워져있을 경우
+1. 이웃이 하나 혹은 없을 경우 세포는 죽는다.
+2. 네 개 이상의 이웃을 가진 셀은 죽는다.
+3. 두 개 혹은 세 개의 이웃이있는 셀은 살아남는다.
+
+공간이 비워져 있을 경우
+4. 각 셀의 이웃이 세 개일 경우 세포는 살아난다.
 """
 
 
-def change_generation(prev_cell):
-    # 세대에 따라 셀의 변화
-    # 네 개의 조건을 바탕으로 알고리즘 구현
-    next_cell = []
-    return next_cell
+def change_generation(cell_list):
+    """
+    :param <list> 다음 세대로 변하기 전 살아 있는 세포들의 좌표 리스트
+    """
+    # col부터 진행 -> cache hit 유도
+    for col in range(len(MAIN_GRID)):
+        for row, cell in enumerate(MAIN_GRID[col]):
+            live_neighbor_cell_num = check_live_neighbor(row, col)
+            if '■' == cell:
+                if live_neighbor_cell_num == 1 or live_neighbor_cell_num == 0:  # 조건1
+                    MAIN_GRID[row][col] = '□'  # die
+                elif live_neighbor_cell_num > 3:  # 조건2
+                    MAIN_GRID[row][col] = '□'  # die
+                elif live_neighbor_cell_num == 2 or live_neighbor_cell_num == 3:  # 조건3
+                    continue  # 살아있는 상태 유지
+            else:
+                if live_neighbor_cell_num == 3:
+                    MAIN_GRID[col][row] = '■'  # resurrection
 
 
 def visualize():
@@ -137,7 +138,16 @@ if __name__ == '__main__':
     # initialize('plus.txt')  # 파일 이름과 세대를 입력할 경우 -> 파일에 정의된 state
 
     # 초기화
-    s, g, i = initialize()  # 사용자가 값을 입력하지 않을 경우
+    s, g, i = initialize('plus.txt')  # 사용자가 값을 입력하지 않을 경우
     # 그리드 생성
-    result = create_grid(s, i)  # 배열을 이용해 그리드 생성
-    print(result)
+    result = create_grid(i)  # 배열을 이용해 그리드 생성
+
+    test_result1 = []
+    for i in MAIN_GRID:
+        test_result1.append(''.join(i))
+    print(test_result1)
+    change_generation(i)
+    test_result = []
+    for i in MAIN_GRID:
+        test_result.append(''.join(i))
+    print(test_result)
